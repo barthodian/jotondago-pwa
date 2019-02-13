62159d69ca3950f8255e45fce0e2be18592f83c5
Barthodian <barthodian@gmail.com>
2019-02-13 17:58:34 -0600

    added some basic material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+// Material section
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
+// Routing
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
@@ -12,9 +22,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+
+    // Material section
+    
     BrowserAnimationsModule,
     MatButtonModule,
     MatCheckboxModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule, 
     MatToolbarModule
   ],
   providers: [],
---
+++ src/app/orders/orders.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OrdersRoutingModule } from './orders-routing.module';
+import { OrderListComponent } from './order-list/order-list.component';
+
+// Material components
+import { MatTabsModule } from '@angular/material/tabs';
+
+@NgModule({
+  declarations: [OrderListComponent],
+  imports: [
+    CommonModule,
+    OrdersRoutingModule,
+
+    // Material
+
+    MatTabsModule
+  ]
+})
+export class OrdersModule { }
